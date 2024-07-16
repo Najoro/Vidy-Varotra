01842d225a32229e0eb8dc5f9722378fef22af70
Najoro Fanantenana <najofanantenana@gmail.com>
2024-07-16 22:28:46 +0300

    register form , contraints user

--- src/Form/RegisterUserType.php
+++ src/Form/RegisterUserType.php
@@ -3,21 +3,48 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Validator\Constraints\Length;
 
 class RegisterUserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('password')
-            ->add('lastName')
-            ->add('firstName')
-            ->add("save" , SubmitType::class);
+            ->add('email', EmailType::class , [
+                "label" => "E-mail",
+            ])
+            ->add("plainPassword" , RepeatedType::class ,[
+                'type' => PasswordType::class,
+                "first_options" => [
+                    "label" => "Entrer votre mot de passe",
+                    "hash_property_path" => "password",
+                ],
+                "second_options" => [
+                    "label" => "confirmer votre mot de passe"
+                ],
+                "mapped" => false,
+            ])
+            ->add('lastName' , TextType::class , [
+                "label" => "Entrer Nom",
+            ])
+            ->add('firstName' , TextType::class , [
+                "label" => "Entrer Prenom",
+            ])
+            ->add("save" , SubmitType::class ,[
+                "label" => "Enregistrer",
+                "attr" => [
+                    "class" => "btn btn-secondary"
+                ]
+            ]);
         ;
     }
 
