a1bc750e017c8b3b10b1cc1ba039a1ce3f07e62c
Najoro Fanantenana <najofanantenana@gmail.com>
2024-07-16 22:33:54 +0300

    register

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,19 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Form\RegisterUserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class RegisterController extends AbstractController
 {
     #[Route('/inscription', name: 'app_register')]
-    public function index(): Response
+    public function inscription(Request $request , EntityManagerInterface $em): Response
     {
-        $form = $this->createForm(RegisterUserType::class);
-        return $this->render('register/index.html.twig', [
-            "RegisterForm" => $form
+        $user = new User();
+        $form = $this->createForm(RegisterUserType::class , $user);
+
+        $form->handleRequest($request);
+        if($form->isValid() && $form->isSubmitted())
+        {
+            $em->persist($user);
+            $em->flush();
+        }
+
+
+        return $this->render('register/register.html.twig', [
+            "RegisterForm" => $form->createView()
         ]);
     }
 }
