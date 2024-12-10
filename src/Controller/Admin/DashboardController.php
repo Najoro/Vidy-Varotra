f84bb8d57208d79a62bd3517558990b8d2154f7a
najoro Fanantenana <najofanantenana@gmail.com>
2024-12-10 19:16:32 +0100

    add new entiry category and product

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Category;
+use App\Entity\Product;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -44,5 +46,7 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Utilisateur', 'fas fa-list', User::class);
+        yield MenuItem::linkToCrud('Categorie', "fa fa-list", Category::class);
+        yield MenuItem::linkToCrud('Produit', "fa fa-list", Product::class);
     }
 }
